18249d4e12e994d4fe40018ede510ec5530e1f3c
joha2424 <johannavita31@gmail.com>
2023-09-20 09:03:53 -0500

    20-09-23

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {styles} from './assets/styles/allStyles';
+import Login_screen from './components/Login_screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Login_screen /> 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
